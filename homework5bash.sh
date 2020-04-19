ef8093e3ea076b44bb3f04b402bb75caa1419141
M'Lynn <43681894+vmlynnj@users.noreply.github.com>
2020-04-19 11:12:42 -0400

    Update homework5bash.sh

--- homework5bash.sh
+++ homework5bash.sh
@@ -7,11 +7,11 @@ wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chrX.fa.gz
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chrY.fa.gz
 gunzip *
 git clone https://github.com/vmlynnj/homework5jar.git
-hdfs dfs -mkrdir /user/homework5Input
-for x in *.fa; do hdfs dfs -put $x homework5Input; done;
 export PATH=${JAVA_HOME}/bin:${PATH}
 echo $PATH
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 echo $HADOOP_CLASSPATH
-cd ./homework5Input
-hadoop jar homework5.jar KmerDriver homework5Input homework5Output 5
+hdfs dfs -mkrdir /user/homework5Input
+for x in *.fa; do hdfs dfs -put $x homework5Input; done;
+
+
